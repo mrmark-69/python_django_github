83ceed3e53d0b3db98fd306e8d7272111a4b7a91
Марк Кузнецов <mrmark@yandex.ru>
2023-07-12 22:27:43 +1000

    Поэкспериментировал с html-разметкой

---
+++ mysite/shopapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from timeit import default_timer
+
+
+def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+        'django': 'the web framework for perfectionists with deadlines',
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
