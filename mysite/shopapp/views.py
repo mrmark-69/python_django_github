1b143359895447994e4716933488c36f5f51be74
Марк Кузнецов <mrmark@yandex.ru>
2023-07-14 14:07:49 +1000

    Промежуточный коммит

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from timeit import default_timer
+from django.utils import timezone
+
 
 
 def shop_index(request: HttpRequest):
@@ -10,8 +12,9 @@ def shop_index(request: HttpRequest):
         ('Smartphone', 999),
     ]
     context = {
+        "time_now": timezone.now(),
         "time_running": default_timer(),
         "products": products,
-        'django': 'the web framework for perfectionists with deadlines',
+        "header": "hello shop index",
     }
     return render(request, 'shopapp/shop-index.html', context=context)
