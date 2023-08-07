a85acc9fb0c3cbb7cf33e0283a6d85f00a36d7ec
mrmark <mrmark@yandex.ru>
2023-08-08 00:25:53 +1000

    stable state

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'shopapp.apps.ShopappConfig',
+    'requestdataapp.apps.RequestdataappConfig',
 ]
 
 MIDDLEWARE = [
---
+++ mysite/requestdataapp/views.py
@@ -0,0 +1,34 @@
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def process_get_weiv(request: HttpRequest) -> HttpResponse:
+    a = request.GET.get("a", "")
+    b = request.GET.get("b", "")
+    result = a + b
+
+    context = {
+        "a": a,
+        "b": b,
+        "result": result,
+    }
+    return render(request, "requestdataapp/request-query-params.html", context=context)
+
+
+def user_form(request: HttpRequest) -> HttpResponse:
+    return render(request, "requestdataapp/user-bio-form.html")
+
+
+def handle_file_upload(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST" and request.FILES.get("myfile"):
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        if myfile.size > 2.5e6:
+            print("File size exceeded")
+        else:
+            print("Allowed file size")
+            filename = fs.save(myfile.name, myfile)
+            print(f"Saved file {filename}")
+    return render(request, "requestdataapp/file-upload.html")
