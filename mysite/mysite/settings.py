afe78007ba2bcde4d5869067c6e87359018d9779
mrmark <mrmark@yandex.ru>
2023-08-08 00:28:28 +1000

    middlewares added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'requestdataapp.middlewares.set_useragent_on_request_middleware',
+    'requestdataapp.middlewares.CountRequestsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
