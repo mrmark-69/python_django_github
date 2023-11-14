a1cbc74d4e85c278c9ebd456a1962fd730589ba4
mrmark <mrmark@yandex.ru>
2023-11-15 08:13:05 +1000

    before merge

--- mysite/shopapp/management/commands/agg.py
+++ mysite/shopapp/management/commands/agg.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
         # )
         # print(result)
         orders = Order.objects.annotate(
-            total=Sum('products__price'),
+            total=Sum('products__price', default=0),
             products_count=Count('products'),
         )
         for order in orders:
