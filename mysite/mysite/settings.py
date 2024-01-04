3210f4fb55c75dc7a725e504f9b6fdd096fc38bb
mrmark <mrmark@yandex.ru>
2024-01-04 21:02:28 +1000

    откат

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -195,7 +195,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
